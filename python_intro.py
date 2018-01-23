258c2fcbd6e292b27a82c11e43d0f9361c12f531
Beenita <Beenita.shah@hotmail.co.uk>
2018-01-23 23:11:42 +0000

    Django Girls app, Initial commit

---
+++ python_intro.py
@@ -0,0 +1,20 @@
+print("Hello, Django Girls!")
+
+if 3>2:
+    print("3 is greater than 2")
+else:
+    print("Math is broken")
+
+def hi():
+    print("Hi there")
+    print("How are you?!?!")
+
+def hii(name):
+    if name == 'Ola':
+        print('Hi Ola!')
+    elif name == 'Sonja':
+        print('Hi Sonja!')
+    else:
+        print('Hi anonymous!')
+
+hii("Ola")
